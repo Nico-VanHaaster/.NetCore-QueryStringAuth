6ebbe685e6b049465e28656ee825d1b0cfacb597
Nico VanHaaster <nico.vanhaaster@iseeuglobal.com>
2017-08-21 14:20:05 +0930

    Commit of initial repository

---
+++ KeyAuthTest/QueryStringAuthHandler.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace KeyAuthTest
+{
+    public class QueryStringAuthOptions : AuthenticationOptions
+    {
+        public const string QueryStringAuthSchema = "QueryStringAuth";
+        public const string QueryStringAuthClaim = "QueryStringKey";
+
+        public QueryStringAuthOptions()
+        {
+            AuthenticationScheme = QueryStringAuthSchema;
+        }
+
+        public string QueryStringKeyParam { get; set; } = "key";
+
+        public string ClaimsTypeName { get; set; } = "QueryStringKey";
+
+        public AuthenticationProperties AuthenticationProperties { get; set; } = new AuthenticationProperties();
+    }
+
+    public class QueryStringAuthHandler : AuthenticationHandler<QueryStringAuthOptions>
+    {
+        /// <summary>
+        /// Handle authenticate async
+        /// </summary>
+        /// <returns></returns>
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (Request.Query.TryGetValue(Options.QueryStringKeyParam, out StringValues value) && value.Count > 0)
+            {
+                var key = value[0];
+
+                //..do your authentication...
+
+                if (!string.IsNullOrWhiteSpace(key))
+                {
+                    //setup you claim
+                    var claimsPrinciple = new ClaimsPrincipal();
+                    claimsPrinciple.AddIdentity(new ClaimsIdentity(new[] { new Claim(Options.ClaimsTypeName, key) }, Options.AuthenticationScheme));
+
+                    //create the result ticket
+                    var ticket = new AuthenticationTicket(claimsPrinciple, Options.AuthenticationProperties, Options.AuthenticationScheme);
+                    var result = AuthenticateResult.Success(ticket);
+                    return Task.FromResult(result);
+                }
+            }
+            return Task.FromResult(AuthenticateResult.Fail("Key not found or not valid"));
+
+        }
+    }
+
+    public class QueryStringAuthMiddleware : AuthenticationMiddleware<QueryStringAuthOptions>
+    {
+        public QueryStringAuthMiddleware(RequestDelegate next, IOptions<QueryStringAuthOptions> options, ILoggerFactory loggerFactory, UrlEncoder encoder)
+            : base(next, options, loggerFactory, encoder)
+        {
+        }
+
+        protected override AuthenticationHandler<QueryStringAuthOptions> CreateHandler()
+        {
+            return new QueryStringAuthHandler();
+        }
+    }
+
+
+    public static class QueryStringAuthMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseQueryStringAuthentication(this IApplicationBuilder appBuilder)
+        {
+            if (appBuilder == null)
+                throw new ArgumentNullException(nameof(appBuilder));
+
+            var options = new QueryStringAuthOptions();
+            return appBuilder.UseQueryStringAuthentication(options);
+        }
+
+        public static IApplicationBuilder UseQueryStringAuthentication(this IApplicationBuilder appBuilder, Action<QueryStringAuthOptions> optionsAction)
+        {
+            if (appBuilder == null)
+                throw new ArgumentNullException(nameof(appBuilder));
+
+            var options = new QueryStringAuthOptions();
+            optionsAction?.Invoke(options);
+            return appBuilder.UseQueryStringAuthentication(options);
+        }
+
+        public static IApplicationBuilder UseQueryStringAuthentication(this IApplicationBuilder appBuilder, QueryStringAuthOptions options)
+        {
+            if (appBuilder == null)
+                throw new ArgumentNullException(nameof(appBuilder));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return appBuilder.UseMiddleware<QueryStringAuthMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ KeyAuthTest/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace KeyAuthTest
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization(o =>
+            {
+                //override the default policy
+                o.DefaultPolicy = new AuthorizationPolicy(new[] { new ClaimsAuthorizationRequirement(QueryStringAuthOptions.QueryStringAuthClaim, new string[0]) }, new[] { QueryStringAuthOptions.QueryStringAuthSchema });
+
+                //or add a policy
+                //o.AddPolicy("QueryKeyPolicy", options =>
+                //{
+                //    options.RequireClaim(QueryStringAuthOptions.QueryStringAuthClaim);
+                //    options.AddAuthenticationSchemes(QueryStringAuthOptions.QueryStringAuthSchema);
+                //});
+
+            });
+            services.AddAuthentication(o =>
+            {
+                o.SignInScheme = QueryStringAuthOptions.QueryStringAuthSchema;
+            }); //adds the auth services
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+
+            app.UseQueryStringAuthentication(); //add our query string auth
+
+            //add mvc last
+            app.UseMvc();
+        }
+    }
+}
